ebb1fc274a6c925f27cebe3cbc74569bc8b43818
waricopy <lewismw9@gmail.com>
2019-12-26 08:46:18 +0900

    ダウンロードのベンチマークも追加

--- main_test.go
+++ main_test.go
@@ -11,31 +11,11 @@ import (
 	"testing"
 )
 
-func BenchmarkUpload(b *testing.B) {
-
-	var s uploadSetting
-
-	s.initForBench()
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		s.comSet.makeTempDir()
-		s.sssaCreate()
-		s.encrypt()
-		s.comSet.makeWriteDir()
-		s.addToIPFS()
-		s.writeConfig()
-	}
-	b.StopTimer()
-
-	s = uploadSetting{}
-}
-
 func BenchmarkSssaCreate(b *testing.B) {
 
 	var s uploadSetting
 
-	s.initForBench()
+	s.initForUpBench()
 	s.comSet.makeTempDir()
 
 	b.ResetTimer()
@@ -51,7 +31,7 @@ func BenchmarkEncrypt(b *testing.B) {
 
 	var s uploadSetting
 
-	s.initForBench()
+	s.initForUpBench()
 	s.comSet.makeTempDir()
 	s.sssaCreate()
 
@@ -68,7 +48,7 @@ func BenchmarkAddToIPFS(b *testing.B) {
 
 	var s uploadSetting
 
-	s.initForBench()
+	s.initForUpBench()
 	s.comSet.makeTempDir()
 	s.sssaCreate()
 	s.encrypt()
@@ -83,17 +63,35 @@ func BenchmarkAddToIPFS(b *testing.B) {
 	s = uploadSetting{}
 }
 
-func (s *uploadSetting) initForBench() {
+func BenchmarkUpload(b *testing.B) {
+
+	var s uploadSetting
+
+	s.initForUpBench()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s.comSet.makeTempDir()
+		s.sssaCreate()
+		s.encrypt()
+		s.comSet.makeWriteDir()
+		s.addToIPFS()
+		s.writeConfig()
+	}
+	b.StopTimer()
+
+	s = uploadSetting{}
+}
+
+func (s *uploadSetting) initForUpBench() {
 
 	var (
-		filePath = []string{
-			"./test-keys/a-pub.pem",
-			"./test-keys/b-pub.pem",
-			"./test-keys/c-pub.pem"}
-		ddArgs = []string{
-			"if=/dev/zero",
+		shareNum int = 3
+		filePath     = []string{"./test-keys/a-pub.pem"}
+		ddArgs       = []string{
+			"if=/dev/urandom",
 			"of=",
-			"bs=10",
+			"bs=100",
 			"count=1",
 		}
 	)
@@ -116,14 +114,15 @@ func (s *uploadSetting) initForBench() {
 	}
 
 	// askShareNum()
-	s.shareNum = len(filePath)
+	s.shareNum = shareNum
 
 	// askMinNum()
-	s.minNum = len(filePath)
+	s.minNum = shareNum
 
 	// askShareManagers()
 	for i := 0; i < len(filePath); i++ {
-		s.managers[i].manageShareNum = 1
+		s.managers[i].manageShareNum = shareNum
+		s.cipherShareNum += s.managers[i].manageShareNum
 	}
 
 	// askFilePath()
@@ -137,13 +136,129 @@ func (s *uploadSetting) initForBench() {
 	}
 
 	// ディレクトリの削除
-	if _, err := os.Stat("temp"); os.IsExist(err) {
+	if _, err = os.Stat("temp"); err == nil {
 		if err := os.RemoveAll("temp"); err != nil {
 			log.Fatal(err)
 		}
 	}
-	if _, err := os.Stat("outputs"); os.IsExist(err) {
+	if _, err = os.Stat("outputs"); err == nil {
 		if err := os.RemoveAll("outputs"); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkGetFromIPFS(b *testing.B) {
+
+	var s downloadSetting
+
+	s.initForDownBench()
+	s.comSet.makeTempDir()
+	s.readConfig()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s.getFromIPFS()
+	}
+	b.StopTimer()
+
+	s = downloadSetting{}
+}
+
+func BenchmarkDecrypt(b *testing.B) {
+
+	var s downloadSetting
+
+	s.initForDownBench()
+	s.comSet.makeTempDir()
+	s.readConfig()
+	s.getFromIPFS()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s.dencrypt()
+	}
+	b.StopTimer()
+
+	s = downloadSetting{}
+}
+
+func BenchmarkSssaCombine(b *testing.B) {
+
+	var s downloadSetting
+
+	s.initForDownBench()
+	s.comSet.makeTempDir()
+	s.readConfig()
+	s.getFromIPFS()
+	s.dencrypt()
+	s.comSet.makeWriteDir()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s.sssaCombine()
+	}
+	b.StopTimer()
+
+	s = downloadSetting{}
+}
+
+func BenchmarkDownload(b *testing.B) {
+
+	var s downloadSetting
+
+	s.initForDownBench()
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		s.comSet.makeTempDir()
+		s.readConfig()
+		s.getFromIPFS()
+		s.dencrypt()
+		s.comSet.makeWriteDir()
+		s.sssaCombine()
+	}
+	b.StopTimer()
+
+	s = downloadSetting{}
+}
+
+func (s *downloadSetting) initForDownBench() {
+
+	var filePath string = "./test-keys/a.pem"
+
+	// askPrivKeys()
+	file, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	block, _ := pem.Decode(file)
+	var privKey *rsa.PrivateKey
+	switch block.Type {
+	case "RSA PRIVATE KEY":
+		privKey, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+		if err != nil {
+			log.Fatal(err)
+		}
+	default:
+		privKeyInterface, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+		if err != nil {
+			log.Fatal(err)
+		}
+		privKey, _ = privKeyInterface.(*rsa.PrivateKey)
+	}
+	fileStat, err := os.Stat(filePath)
+	s.manager = keyManager{keyfileName: fileStat.Name(), privateKey: privKey}
+
+	// askFilePath()
+	s.comSet.readFilePath = "./outputs/a-pub_pem/config.json"
+
+	// ディレクトリの削除
+	if err := os.RemoveAll("temp"); err != nil {
+		log.Fatal(err)
+	}
+	if _, err = os.Stat("outputs/content"); err == nil {
+		if err := os.Remove("outputs/content"); err != nil {
 			log.Fatal(err)
 		}
 	}
